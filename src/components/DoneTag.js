58cf6f6f9b3de37b8b094407e397553e83d91564
Matan Weiss <matan1111@gmail.com>
2022-02-05 12:48:57 +0200

    fixed DoneTag checkmark

--- src/components/DoneTag.js
+++ src/components/DoneTag.js
@@ -1,7 +1,9 @@
+import { ReactComponent as CheckSVG } from '../assets/check.svg';
+
 const DoneTag = () => {
     return (
-        <div className='btn ml-2 self-start bg-green-600 text-sm inline p-1' >
-            done✔
+        <div className='btn ml-2 self-start bg-green-600 text-sm flex items-center p-1' >
+            done <CheckSVG className="w-4 h-6" />
         </div>
     );
 }
